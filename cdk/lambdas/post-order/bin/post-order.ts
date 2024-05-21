0ec1c55bbf31335b8af55ba22fc9d79f370c336e
Max Halty <halty.maximo@principal.com>
2024-05-21 17:46:34 -0300

    added cdk for lambda

---
+++ cdk/lambdas/post-order/bin/post-order.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { PostOrderStack } from '../lib/post-order-stack';
+import { TagsHelpers } from './helpers/tag-helpers';
+
+if (process.env.AWS_DEPLOY_REGION === undefined) {
+  throw new Error("AWS_DEPLOY_REGION environment variable missing");
+}
+
+if (process.env.AWS_DR_REGION === undefined) {
+  throw new Error("AWS_DR_REGION environment variable missing");
+}
+
+const app = new cdk.App();
+
+const _postOrderStack = new PostOrderStack(app, 'post-order-stack');
+
+TagsHelpers.addTags(_postOrderStack);
